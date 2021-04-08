19cfc803a23669ad414c222382ef88360f6551b1
aejmmark <aejmelaeus.markus@gmail.com>
2021-04-08 20:09:52 +0300

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,36 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    created TIMESTAMP
+);
+
+CREATE TABLE subjects (
+    id SERIAL PRIMARY KEY,
+    secret BOOLEAN,
+    subject TEXT
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    subject_id INTEGER REFERENCES subjects,
+    user_id INTEGER REFERENCES users,
+    topic TEXT,
+    created TIMESTAMP
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    thread_id INTEGER REFERENCES threads,
+    user_id  INTEGER REFERENCES users,
+    comment TEXT,
+    posted TIMESTAMP
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    thread_id INTEGER REFERENCES threads,
+    comment_id INTEGER REFERENCES comments
+);
